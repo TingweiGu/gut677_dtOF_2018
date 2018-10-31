eb8e8f52fdbdb5720514f13564e3cb757d8e048b
Tingwei Gu <gut677@newschool.edu>
2018-10-30 20:08:22 -0400

    update

--- midterm/src/ofApp.h
+++ midterm/src/ofApp.h
@@ -27,7 +27,7 @@ class ofApp : public ofBaseApp{
         int lerpStep = 1;
         int radius;
     
-        string name[27] = {"door", "door2", "shelf", "shelf2", "set1", "set2", "tv", "tv2", "shoe", "shoe2", "sofa", "sofa2", "kitchen", "kitchen2", "chair", "chair2", "desk", "desk2", "table", "table2", "coffee", "coffee2", "game", "game2", "ball", "cart", "cart2"};
+        string name[27] = {"door", "door2", "shelf", "shelf2", "set", "set2", "tv", "tv2", "shoe", "shoe2", "sofa", "sofa2", "kitchen", "kitchen2", "chair", "chair2", "desk", "desk2", "table", "table2", "coffee", "coffee2", "game", "game2", "ball", "cart", "cart2"};
     
         int posX[27] = {1320, 610, 730, 10, 830, 110, 980, 260, 1287, 570, 960, 240, 1150, 430, 1125, 405, 730, 10, 730, 10, 728, 8, 790, 100, 1000, 20, 740};
         int posY[27] = {29, 29, 12, 12, 40, 40, 31, 31, 110, 110, 200, 200, 550, 550, 400, 400, 130, 130, 588, 588, 170, 170, 350, 350, 450, 480, 480};
